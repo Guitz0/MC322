89804b438c7d34a62ab61fd55dab0fe97643221c
Guilherme Silva de Paula <g115832@dac.unicamp.br>
2026-03-12 15:28:02 -0300

    implementa setters

--- tarefa1/tarefa1/src/CartaEscudo.java
+++ tarefa1/tarefa1/src/CartaEscudo.java
@@ -6,9 +6,17 @@ public class CartaEscudo {
     public String getNome() {
         return nome;
     }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public int getCusto() {
         return custo;
+    }
+
+    public void setCusto(int custo) {
+        this.custo = custo;
     }
 
     public void usar(Heroi alvo){
