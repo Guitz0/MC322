7035bdb1c711c25bfd420ac66cc63f83a654e7c9
Guilherme Silva de Paula <g115832@dac.unicamp.br>
2026-03-12 21:21:02 -0300

    Implementa a instanciacao das classes e a batalha pelo terminal

--- tarefa1/tarefa1/src/App.java
+++ tarefa1/tarefa1/src/App.java
@@ -2,17 +2,64 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        int acao;
-
-        Heroi herói = new Heroi();
-        Inimigo rato = new Inimigo();
-        CartaDano cartaDano = new CartaDano();
-        CartaEscudo cartaEscudo = new CartaEscudo();
-
+        int acao, turno = 1;
         Scanner entrada = new Scanner(System.in);
-        acao = entrada.nextInt();
 
+        // Instanciação das Classes por metodos construtores
+        Heroi heroi = new Heroi("Guerreiro", 30, 3);
+        Inimigo inimigo = new Inimigo("Rato", 12, 0, 6);
+        CartaDano cartaDano = new CartaDano("Espada", 1, 5);
+        CartaEscudo cartaEscudo = new CartaEscudo("Escudo", 1, 6);
 
+        System.out.println("====================");
+        System.out.println(heroi.getNome() + " (" + heroi.getVidaAtual() + "/" + heroi.getVidaMaxima() + " de vida)" + " (" + heroi.getEscudo() + " de escudo)");
+        System.out.println("vs");
+        System.out.println(inimigo.getNome() + " (" + inimigo.getVidaAtual() + "/" + inimigo.getVidaMaxima() + " de vida)" + " (" + inimigo.getEscudo() + " de escudo)");
+        System.out.println();
+
+        while (heroi.estaVivo() && inimigo.estaVivo()) {
+            
+            System.out.println("Turno: " + turno);
+            System.out.println();
+            System.out.println(heroi.getEnergiaAtual() + "/" + heroi.getEnergiaMaxima() + " de Energia disponível");
+            System.out.println("Escolha uma ação:");
+            System.out.println("1 - Usar carta" + cartaDano.getNome());
+            System.out.println("2 - Usar carta" + cartaEscudo.getNome());
+            System.out.println("3 - Encerrar turno");
+            System.out.println("====================");
+
+            do {
+                
+                acao = entrada.nextInt();
+
+                switch (acao) {
+                    case 1:
+                        cartaDano.usar(inimigo);
+                        heroi.usarEnergia(cartaDano.getCusto());
+                        System.out.println("Causou dano");
+                        break;
+                    case 2:
+                        cartaEscudo.usar(heroi);
+                        heroi.usarEnergia(cartaEscudo.getCusto());
+                        System.out.println("Recebeu escudo");
+                        break;
+                    default:
+                        break;
+                }
+
+            } while ((heroi.getEnergiaAtual() > 0) && (acao != 3));
+
+            inimigo.atacar(heroi);
+            System.out.println("Recebeu 6 de dano");
+
+            // Zera o escudo do heroi
+            heroi.ganharEscudo(-heroi.getEscudo());
+
+            // Reseta a energia do heroi
+            heroi.usarEnergia(-heroi.getEnergiaMaxima());
+            
+        }
         entrada.close();
+        System.out.println("Fim");
     }
 }
