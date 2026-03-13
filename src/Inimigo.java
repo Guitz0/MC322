76b0d31de4fb98e656e4a776df06ab4a5906572e
Guilherme Silva de Paula <g115832@dac.unicamp.br>
2026-03-12 23:47:41 -0300

    Entrega da tarefa1

---
+++ src/Inimigo.java
@@ -0,0 +1,51 @@
+public class Inimigo {
+    private String nome;
+    private int vidaMaxima;
+    private int vidaAtual;
+    private int escudo;
+    private int dano;
+
+    public Inimigo(String nome, int vida, int escudo, int dano) {
+        this.nome = nome;
+        this.vidaMaxima = vida;
+        this.vidaAtual = vida;
+        this.escudo = escudo;
+        this.dano = dano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getVidaMaxima() {
+        return vidaMaxima;
+    }
+
+    public int getVidaAtual() {
+        return vidaAtual;
+    }
+
+    public int getEscudo() {
+        return escudo;
+    }
+
+    public int getDano() {
+        return dano;
+    }
+    
+    public void receberDano(int dano) {
+        vidaAtual = vidaAtual - dano;
+    }
+
+    public void atacar(Heroi alvo) {
+        alvo.receberDano(dano);
+    }
+
+    public boolean estaVivo() {
+        if (vidaAtual > 0)
+            return true;
+        else
+            return false;
+    }
+
+}
