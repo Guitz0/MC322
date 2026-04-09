6f289d45a6d1ab0a88d3739b36d46e91cc71ad60
Guilherme Silva de Paula <g115832@dac.unicamp.br>
2026-04-09 17:16:19 -0300

    Altera as classes Heroi e Inimigo para herdarem Entidade

--- tarefa2/src/Heroi.java
+++ tarefa2/src/Heroi.java
@@ -1,34 +1,12 @@
-public class Heroi {
-    private String nome;
-    private int vidaMaxima;
-    private int vidaAtual;
-    private int escudo = 0;
+public class Heroi extends Entidade {
     private int energiaAtual;
     private int energiaMaxima;
 
 
-    public Heroi(String nome, int vida, int energia) {
-        this.nome = nome;
-        this.vidaMaxima = vida;
-        this.vidaAtual = vida;
+    public Heroi(String nome, int vida, int escudo, int energia) {
+        super(nome, vida, escudo);
         this.energiaMaxima = energia;
         this.energiaAtual = energia;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getVidaMaxima() {
-        return vidaMaxima;
-    }
-    
-    public int getVidaAtual() {
-        return vidaAtual;
-    }
-
-    public int getEscudo() {
-        return escudo;
     }
 
     public int getEnergiaMaxima() {
@@ -37,24 +15,6 @@ public class Heroi {
 
     public int getEnergiaAtual() {
         return energiaAtual;
-    }
-
-    public void receberDano(int dano) {
-        if (escudo < dano)
-            vidaAtual = vidaAtual + escudo - dano;
-        else
-            escudo = escudo - dano;
-    }
-
-    public void ganharEscudo(int valor) {
-        escudo = escudo + valor;
-    }
-
-    public boolean estaVivo() {
-        if (vidaAtual > 0)
-            return true;
-        else
-            return false;
     }
 
     public void usarEnergia(int custo){
