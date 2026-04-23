a277b7378f1ee27e5a7e61c33fa56bd24adb12bb
Guilherme Silva de Paula <g115832@dac.unicamp.br>
2026-04-22 22:11:56 -0300

    Altera a classe CartaDano para herdar de Carta

--- tarefa2/src/CartaDano.java
+++ tarefa2/src/CartaDano.java
@@ -1,27 +1,19 @@
-public class CartaDano {
-    private String nome;
-    private int custo;
+public class CartaDano extends Carta {
     private int dano;
 
-    public CartaDano(String nome, int custo, int dano){
-        this.nome = nome;
-        this.custo = custo;
+    public CartaDano(String nome, String descricao, int custo, int dano) {
+        super(nome, descricao, custo);
         this.dano = dano;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getCusto() {
-        return custo;
     }
 
     public int getDano() {
         return dano;
     }
 
-    public void usar(Inimigo alvo){
-        alvo.receberDano(dano);
+    @Override
+    public void usar(Heroi heroi, Inimigo inimigo){
+        heroi.usarEnergia(custo);
+        inimigo.receberDano(dano);
     }
+    
 }
